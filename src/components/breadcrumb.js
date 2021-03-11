34e3cf52b7fbbbe442c582ab7f6ea0a1ccf669ec
jangjunhyeok <95jjangjun@gmail.com>
2021-03-11 22:29:33 +0900

    feat: add BreadCrumb

--- src/components/breadcrumb.js
+++ src/components/breadcrumb.js
@@ -1,3 +1,22 @@
 export default class BreadCrumb {
-  constructor() {}
+  constructor() {
+    this.breadCrumb = document.querySelector(".Breadcrumb");
+    this.path = ["root"];
+    this.render();
+  }
+
+  setState(next) {
+    this.path.push(next);
+    this.render();
+  }
+
+  render() {
+    this.breadCrumb.innerHTML = "";
+    this.path.forEach((name) => {
+      const cur = document.createElement("div");
+      cur.className = name;
+      cur.textContent = name;
+      this.breadCrumb.appendChild(cur);
+    });
+  }
 }
