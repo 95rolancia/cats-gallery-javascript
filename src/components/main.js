4394081fa4ca6833dc683127a4afb6515b4b1a11
jangjunhyeok <95jjangjun@gmail.com>
2021-05-11 23:28:07 +0900

    Add main.js

--- src/components/main.js
+++ src/components/main.js
@@ -1,7 +1,3 @@
-import BreadCrumb from "./breadcrumb.js";
-import Nodes from "./nodes.js";
-import Loading from "../utils/loading.js";
+import App from "./app.js";
 
-const loading = new Loading();
-const breadCrumb = new BreadCrumb();
-const nodes = new Nodes(loading, breadCrumb);
+new App(document.querySelector("#app"));
