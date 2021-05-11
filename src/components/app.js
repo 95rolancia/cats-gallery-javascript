ad1125c29db471cf6d25706654f58b96d01d77c4
jangjunhyeok <95jjangjun@gmail.com>
2021-05-11 23:27:46 +0900

    Update app.js about Nodes for refactoring

--- src/components/app.js
+++ src/components/app.js
@@ -18,8 +18,46 @@ export default class App {
     this.nodes = new Nodes({
       $app,
       initialState: {
-        isRoot: this.state.isRoot,
-        nodes: this.state.nodes,
+        ...this.state,
+      },
+      async onClick(node) {
+        console.log(this.state);
+        if (node.type === "DIRECTORY") {
+          const nextNodes = await Api.request(node.id);
+          this.setState({
+            ...this.state,
+            depth: [...this.state.depth, node],
+            isRoot: false,
+            nodes: nextNodes,
+          });
+        } else if (node.type === "FILE") {
+          console.log("file");
+        }
+      },
+      async onBackClick() {
+        console.log(this.state);
+        const nextState = { ...this.state };
+        nextState.depth.pop();
+
+        const prevNodeId =
+          nextState.depth.length === 0 ? null : nextState.depth[nextState.depth.length - 1].id;
+
+        if (prevNodeId === null) {
+          const rootNodes = await Api.request();
+          this.setState({
+            ...nextState,
+            isRoot: true,
+            nodes: rootNodes,
+          });
+        } else {
+          const prevNodes = await Api.request(prevNodeId);
+          this.setState({
+            ...nextState,
+            isRoot: false,
+            nodes: prevNodes,
+          });
+        }
+        console.log(this.state);
       },
       Loading,
     });
@@ -30,13 +68,14 @@ export default class App {
     this.state = nextState;
     this.breadCrumb.setState(this.state.depth);
     this.nodes.setState({
+      depth: this.state.depth,
       isRoot: this.state.isRoot,
       nodes: this.state.nodes,
     });
   }
 
   async init() {
-    const rootNodes = await Api.fetchRoot();
+    const rootNodes = await Api.request();
     this.setState({
       ...this.state,
       isRoot: true,
