b4aae81643575306386eb67f4ab8fbd393085511
jangjunhyeok <95jjangjun@gmail.com>
2021-05-13 14:15:43 +0900

    api.js class화

--- src/api/api.js
+++ src/api/api.js
@@ -1,39 +1,17 @@
-const ROOT_URL = "public/data";
-const IMG_URL = "public/imgs/cats";
-
+const API_END_POINT = "public/data";
 const delay = (ms = 10) => new Promise((r) => setTimeout(r, ms));
 
-const api = {
-  request: async (nodeId) => {
+export default class Api {
+  async request(nodeId) {
     try {
       await delay();
-      const res = await fetch(`${ROOT_URL}/${nodeId ? nodeId : "content"}.json`);
+      const res = await fetch(`${API_END_POINT}/${nodeId ? nodeId : "content"}.json`);
       if (!res.ok) {
-        throw new Error("서버의 상태가 이상합니다!");
+        throw new Error("서버의 상태가 이상합니다!", res);
       }
       return await res.json();
-    } catch (e) {
-      throw new Error("무언가가 잘못되었습니다!", e);
+    } catch (err) {
+      throw new Error("무언가가 잘못되었습니다!", err);
     }
-  },
-
-  fetchPng: async (filePath) => {
-    try {
-      window.open(filePath);
-      console.log(filePath);
-      const result = await fetch(`${filePath}`);
-
-      return {
-        isError: false,
-        data: result,
-      };
-    } catch (e) {
-      return {
-        isError: true,
-        data: e,
-      };
-    }
-  },
-};
-
-export default api;
+  }
+}
